7a2537c5c5a6b6f210e71c7e249e9c67c10bfeb0
Miki Pregaard <48087675+Miki0110@users.noreply.github.com>
2023-06-08 19:21:20 +0200

    Woops krnl for 7 was the wrong one here we go :)

--- Lecture 7/krnl_opgave/krnl_opgave.ino
+++ Lecture 7/krnl_opgave/krnl_opgave.ino
@@ -1,150 +1,246 @@
+//220216
 #include <krnl.h>
 
+//////////// TWO TASKS WITH BREAKOUT ///////////////
+/*
 const int STKSZ = 200;
-
-#define LED1 8
-#define LED2 9
 
 struct k_t *pt1,  // pointer to hold reference
   *pt2;           // to taskdescriptor for t1 and t2
 
 char stak1[STKSZ], stak2[STKSZ];
 
-void alg01() {}  // for time being empty
-
-char dataBufForMsgQ[11*10];
-
-struct k_msg_t *msgQ; // Msg queue struct
-
-int loopNr1 = 0;
-int loopNr2 = 0;
-
-void aktuer01() {
-  Serial.print(",");
-  Serial.print("aktuer1:");
-  Serial.println(loopNr1);
-  loopNr1 = loopNr1 + 1;
-}
-
-void aktuer02() {
-  Serial.print("aktuer2:");
-  Serial.println(loopNr2);
-  loopNr2 = loopNr2 + 1;
-}
-
-// Assignment 1 tasks
 void t1(void) {
-
   while (1) {
-    k_sleep(50);
-    k_eat_msec(5000);
-    //aktuer01();
-    k_breakout();
+    k_sleep(10);
+    k_eat_msec(5); // you can't really see the LED without this
   }
 }
+
+
 
 void t2() {
   while (1) {
-    digitalWrite(12, !digitalRead(13));
-    //Serial.println(!digitalRead(13));
-    //aktuer02();
-    k_sleep(100);
+    k_sleep(50); 
+    k_eat_msec(10); // you can't really see the LED without this
   }
 }
-
-// Assignment 3 tasks
-void producerTask(void) {
-  char res;
-  char message[11] ="Hello world";
-  
-  while (1) {
-    for (int i = 0; i < 11; i++){
-      res = k_send(msgQ, &message[i]);
-    }
-    k_sleep(2000);
-    //aktuer01();
-    k_breakout();
-  }
-}
-
-void consumerTask() {
-  char res;
-  char temp;
-  char message[11];
-  int lostM;  
-  int pos = 0;
-  while (1) {
-    digitalWrite(12, !digitalRead(13));
-    res = k_receive(msgQ,  &temp, 0, &lostM);
-      // Successfully received a message
-      message[pos] = temp;
-      pos++;
-
-      if (pos == 11) {
-        // Received entire message
-        Serial.println(message);
-        pos = 0; // Reset position for next message
-      }    
-
-    k_clear_msg_Q(msgQ);
-    //aktuer02();
-    //k_sleep(100);
-
-  }
-}  
 
 void setup() {
 
-  Serial.begin(115200);
-  pinMode(12, OUTPUT);
-  pinMode(13, OUTPUT);  
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
 
-  // init krnl
-  k_init(2, 0, 1);
-  //  length of message v, byte size v, 
-  msgQ = k_crt_send_Q (11, sizeof(char),  dataBufForMsgQ);
-
+  // init krnl so you can create 2 tasks, no semaphores and no message queues
+  k_init(2, 0, 0);
 
   //               |------------ function used for body code for task
-  //               |  |--------- priority (lower number= higher prio
+  //               |  |--------- priority (lower number= higher prio)
   //               |  |   |----- staksize for array s1
   //                         |-- array used for stak
-  pt1 = k_crt_task(producerTask, 5, stak1, STKSZ);
-  pt2 = k_crt_task(consumerTask, 10, stak2, STKSZ);
-
-
-  // NB-1 remember an Arduino has only 2-8 kByte RAM
-  // NB-2 remember that stak is used in function calls for
-  //  - return address
-  //  - registers stakked
-  //  - local variabels in a function
-  //  So having 200 Bytes of stak excludes a local variable like ...
-  //    int arr[400];
-  // krnl call k_unused_stak returns size of unused stak
-  // Both task has same priority so krnl will shift between the
-  // tasks every 10 milli second (speed set in k_start)
-
+  pt1 = k_crt_task(t1, 9, stak1, STKSZ);
+  pt2 = k_crt_task(t2, 10, stak2, STKSZ);
   k_start();  // start kernel with tick speed 1 milli seconds
 }
 
 void loop() {
-  /* loop will never be called */
 }
 
 
 
 extern "C" {
-
 	void k_breakout() // called every task shift from dispatcher
 	{
 		if (pRun->nr == 0)  // 0 is dummy task - the eater of excessive CPU when all user tasks are idling
 		{
-			PORTB = PORTB | B11011111;  // led13 (bit 5) on let the rest be untouched
+			PORTB = PORTB | B00100000;  // led13 (bit 5) on let the rest be untouched
 		}
 		else {
-			PORTB = PORTB & B00100000;  // led13 off uno
+			PORTB = PORTB & B11011111;  // led13 off uno
 		}
 	}
 
+}*/
+
+/*
+//////////// MSGQ ///////////////
+
+#include <krnl.h>
+
+#define STK 150
+char a1[STK], a2[STK];
+
+struct k_t * pTask1, *pTask2;
+
+struct k_msg_t *msgQ;
+
+int dataBufForMsgQ[10]; // 10 ints..
+
+void task1()  // Producer task
+{
+	int i = 0;
+	
+	while (1) {
+		
+		i++;
+		k_send(msgQ, &i);
+		
+		k_sleep(20);  // Periodically runs every 20ms
+		
+	}
 }
 
+void task2()  // Consumer task
+{
+	char res;
+	int ii, lostM;
+	
+	while (1) {
+		
+		res = k_receive(msgQ,  &ii, 0, &lostM);
+		
+		if (res >= 0) {
+			// We've received a message, print it.
+			Serial.print("Received: "); 
+			Serial.println(ii);
+		}
+	}
+}
+
+void setup() {
+	Serial.begin(9600);
+	delay(2000);
+	
+	k_init(2, 0, 1); // 2 tasks, 0 semaphores, 1 message queue 
+
+	msgQ = k_crt_send_Q (10, sizeof(int),  dataBufForMsgQ);  // 10 elements of size
+	
+	pTask1 = k_crt_task(task1, 10, a1, STK); // Producer task with higher priority
+	pTask2 = k_crt_task(task2, 15 , a2, STK); // Consumer task with lower priority
+	
+	k_start();
+	
+}
+
+void loop() {}
+*/
+
+/*
+//////////// PRINT WITHOUT SEMAPHORES ///////////////
+// This does not work perfectly as 1 runs before 2 is done writing 'b' -> giving such a print "23b\n 1a"
+#include <krnl.h>
+
+const int STKSZ = 200;
+
+struct k_t *pt1, *pt2;
+
+char stak1[STKSZ], stak2[STKSZ];
+
+
+void t1(void) {
+  while (1) {
+   // digitalWrite(12, !digitalRead(13));
+    k_sleep(30); // Go every 30 ms
+    for(int i=0; i<50; i++){
+      Serial.print(i+1);
+      Serial.println("a");
+    }
+  }
+}
+
+
+
+void t2() {
+  while (1) {
+   // digitalWrite(12, !digitalRead(13));
+    k_sleep(30); // Go every 30 ms
+    for(int i=0; i<50; i++){
+      Serial.print(i+1);
+      Serial.println("b");
+    }
+  }
+}
+
+void setup() {
+
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+
+  // init krnl so you can create 2 tasks, no semaphores and no message queues
+  k_init(2, 0, 0);
+
+  //               |------------ function used for body code for task
+  //               |  |--------- priority (lower number= higher prio
+  //               |  |   |----- staksize for array s1
+  //                         |-- array used for stak
+  pt1 = k_crt_task(t1, 10, stak1, STKSZ);
+  pt2 = k_crt_task(t2, 11, stak2, STKSZ);
+
+  k_start();
+}
+
+void loop() {
+}
+*/
+
+// This does not work perfectly as they use too much time to run every 30ms
+#include <krnl.h>
+
+const int STKSZ = 200;
+
+struct k_t *pt1, *pt2, *mutSem;
+
+char stak1[STKSZ], stak2[STKSZ];
+
+
+void t1(void) {
+  while (1) {
+   // digitalWrite(12, !digitalRead(13));
+    k_sleep(30); // Go every 30 ms
+    k_wait(mutSem, 0); // wait for semaphore
+    for(int i=0; i<50; i++){
+      Serial.print(i+1);
+      Serial.println("a");
+    }
+    k_signal(mutSem); // release semaphore
+  }
+}
+
+
+
+void t2() {
+  while (1) {
+   // digitalWrite(12, !digitalRead(13));
+    k_sleep(30); // Go every 30 ms
+    k_wait(mutSem, 0); // wait for semaphore
+    for(int i=0; i<50; i++){
+      Serial.print(i+1);
+      Serial.println("b");
+    }
+    k_signal(mutSem); // release semaphore
+  }
+}
+
+void setup() {
+
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+
+  // init krnl so you can create 2 tasks, 1 semaphore and no message queues
+  k_init(2, 1, 0);
+  
+  // create semaphore for mutual exclusion
+  mutSem = k_crt_sem(1, 1); // Initial value 1, max value 1
+
+  //               |------------ function used for body code for task
+  //               |  |--------- priority (lower number= higher prio
+  //               |  |   |----- staksize for array s1
+  //                         |-- array used for stak
+  pt1 = k_crt_task(t1, 10, stak1, STKSZ);
+  pt2 = k_crt_task(t2, 11, stak2, STKSZ);
+
+  k_start();
+}
+
+void loop() {
+}
